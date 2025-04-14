abc0baa421cb3e6f57817be2905111cfb702e265
7HR4IZ3 <gamerxville@gmail.com>
2025-04-14 10:13:33 -0700

    fix: vercel 2
    Signed-off-by: 7HR4IZ3 <gamerxville@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 
@@ -59,11 +57,11 @@ export default function RootLayout({
       <body className={inter.className}>
         <TrpcProvider>
           <AuthProvider>
-            <Header />
-            <main>
-              <Suspense fallback={<Loading />}>{children}</Suspense>
-            </main>
-            <Footer />
+            <Suspense fallback={<Loading />}>
+              <Header />
+              <main>{children}</main>
+              <Footer />
+            </Suspense>
           </AuthProvider>
         </TrpcProvider>
       </body>
