5064587144a37ff44b15465ab07c204199a73e75
7HR4IZ3 <gamerxville@gmail.com>
2025-05-28 07:20:07 -0700

    fix: fixed height for listing card 2
    Signed-off-by: 7HR4IZ3 <gamerxville@gmail.com>

--- src/components/listing/listing-card.tsx
+++ src/components/listing/listing-card.tsx
@@ -77,8 +77,8 @@ const ListingCard: React.FC<{ business: Business; hideButton?: boolean }> = ({
   // }, [business]);
 
   return (
-    <div className="container bg-white rounded-xl shadow-md overflow-hidden flex flex-col md:flex-row p-4 relative gap-4">
-      <div className="relative w-full aspect-video md:aspect-auto md:w-48 shrink-0 h-30vh md:h-auto">
+    <div className="container bg-white rounded-xl shadow-md overflow-hidden flex flex-col md:flex-row relative gap-4 h-auto">
+      <div className="relative w-full aspect-video md:aspect-auto md:w-48 shrink-0">
         {isLoading ? (
           <Skeleton className="w-full h-full rounded-lg" />
         ) : (
